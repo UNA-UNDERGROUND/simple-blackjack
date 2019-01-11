1ca81be91b89c53543d4ef0637fc7d96250778f8
jonathan <jonguz5@hotmail.com>
2019-01-11 14:36:38 -0600

    misc

--- source/source.cpp
+++ source/source.cpp
@@ -2,11 +2,14 @@
 
 
 using std::cout;
+using std::cin;
+using std::string;
 using std::endl;
 
 int main(int argc, char const *argv[]){
 
     cout << "hola mundo" << endl;
+    cin.ignore();
     return 0;
 }
 
