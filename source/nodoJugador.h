0d477d925cd2525ef84eaf11b3784a0d295ef803
ninjaoflight5 <jonguz5@hotmail.com>
2019-01-16 20:20:25 -0600

    implementado nodoJugador,ligeros cambios del nodoJugador

--- source/nodoJugador.h
+++ source/nodoJugador.h
@@ -2,17 +2,18 @@
 #include "jugadorGenerico.h"
 
 
-//esta clase es exactamente igual a la de nodoCarta,la unica diferiencia es la clase que usa como actual
+// esta clase es exactamente igual a la de nodoCarta,la unica diferiencia es la clase que usa como actual(jugador especificamente),
+// tambien este reclama la memoria del objeto actual, en vez de crear una copia
 
 class nodoJugador {
 private:
-	jugador *actual;
+	jugadorGenerico *actual;
 	nodoJugador * siguiente;
 public:
 
-	nodoJugador(jugador *ref, nodoJugador* siguiente);
-	void setActual(jugador *ref);
-	jugador *getActual();
+	nodoJugador(jugadorGenerico *ref, nodoJugador* siguiente);
+	void setActual(jugadorGenerico *ref);
+	jugadorGenerico *getActual();
 	void setSiguiente(nodoJugador* siguiente);
 	nodoJugador* getSiguiente();
 	~nodoJugador();
