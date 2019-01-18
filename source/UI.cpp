806cf426f5927de5c6ce637ce952922f1245e124
ninjaoflight5 <jonguz5@hotmail.com>
2019-01-18 10:49:41 -0600

    Actualizado CMakeList.txt,creado UI.cpp

---
+++ source/UI.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+#include "juego.h"
+#include "jugador.h"
+#include "dealer.h"
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+
+//definiciones comunes de las cartas
+const std::string nombreCarta[]{ "?", "A", "2", "3", "4" ,"5" ,"6" ,"7" ,"8" ,"9" ,"10", "J", "Q", "k" };
+const std::string nombrePalo[]{ "?", "Espadas", "Tréboles", "Corazones", "Diamantes" };
+
+
+
+
+
+int main(int argc, char const *argv[]) {
+
+
+	setlocale(LC_ALL, "es-CR.UTF-8"); // localizacion al español para soportar tildes y caracteres españoles
+
+
+
+
+
+
+	cin.ignore();
+	return 0;
+}
+
