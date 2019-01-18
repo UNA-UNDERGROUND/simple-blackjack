b19f711039da3982642dc2e3ba49471f460fa96b
ninjaoflight5 <jonguz5@hotmail.com>
2019-01-18 11:41:33 -0600

    agregados gotoXY y cambiarColor

--- source/UI.cpp
+++ source/UI.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
 
-#include "juego.h"
-#include "jugador.h"
-#include "dealer.h"
+
+#include "utiles.h"
 
 using std::cout;
 using std::cin;
@@ -18,9 +17,13 @@ int main(int argc, char const *argv[]) {
 	setlocale(LC_ALL, "es-CR.UTF-8"); // localizacion al español para soportar tildes y caracteres españoles
 
 
-
-
-
+	gotoXY(8, 8);
+	color Color = blanco;
+	cambiarColor(celeste, blanco);
+	cout << "hola mundo!!!";
+	//cout << "color: " << "0x" << std::hex << Color << endl;
+	//cout << "color(desplazado): " << "0x" << std::hex << (Color<<4) << endl;
+	//cout << "color: " << "0x" << std::hex << (BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE) << endl;
 
 	cin.ignore();
 	return 0;
--- source/utiles.h
+++ source/utiles.h
@@ -4,19 +4,51 @@
 
 //utiles: aqui se encontraran fragmentos de codigo para la interfaz grafica
 //
-// las funciones fueron basadas en ls
+// las funciones fueron basadas en los siguientes recursos
 //
-// https://www.aulavirtual.una.ac.cr/mod/resource/view.php?id=302651
+// gotoxy extraido de:								https://www.aulavirtual.una.ac.cr/mod/resource/view.php?id=302651
+// color  basado en:								https://stackoverflow.com/questions/9203362/c-color-text-in-terminal-applications-in-windows
+// las definiciones de colores estan basadas en:	https://stackoverflow.com/questions/17125440/c-win32-console-color
+
+// 
+// definiciones de colores, notese que estan limitados a 8 colores,pero son 16 por la intentencidad,tambien al ser codigos hexadecimales pueden ser combinados con un o lógico
+// o desplazarlos para cambiar entre fondo y texto,pero notese que son hexadecimales y no binarios por lo tanto deben de ser desplazados 4 bits y no uno
+enum color {
+	negro			= 0,
+	azulOscuro		= FOREGROUND_BLUE,
+	verdeOscuro		= FOREGROUND_GREEN,
+	celesteOscuro	= FOREGROUND_GREEN | FOREGROUND_BLUE,
+	rojoOscuro		= FOREGROUND_RED,
+	fucsiaOscuro	= FOREGROUND_RED | FOREGROUND_BLUE,
+	amarilloOscuro	= FOREGROUND_RED | FOREGROUND_GREEN,
+	grisOscuro		= FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+	gris			= FOREGROUND_INTENSITY,
+	azul			= FOREGROUND_INTENSITY | FOREGROUND_BLUE,
+	verde			= FOREGROUND_INTENSITY | FOREGROUND_GREEN,
+	celeste			= FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE,
+	rojo			= FOREGROUND_INTENSITY | FOREGROUND_RED,
+	fucsia			= FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE,
+	amarillo		= FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN,
+	blanco			= FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+};
 
 
+// cambia la posicion del cursor (consola) a la ubicacion indicada en coordenadas 2D 
+void gotoXY(int x, int y) {
+	COORD coordenadas;
+	coordenadas.X = x;
+	coordenadas.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordenadas);
+}
 
-void gotoxy(int x, int y) {
-	COORD coord;
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 
-	//GetStdHandle(STD_OUTPUT_HANDLE)metodo de la windows que me permite ubicar a la consola en un lugar y coord es el objeto con los valores
+//en esta funcion no ocupamos restaurar los colores a diferiencia de la original
+void cambiarColor(color texto, color fondo){
+
+	HANDLE consola = GetStdHandle(STD_OUTPUT_HANDLE);							//a diferiencia de gotoxy aqui usaremos el puntero de la consola mas veces
+											
+	SetConsoleTextAttribute(consola, texto | (fondo << 4));
+
 }
 
 
